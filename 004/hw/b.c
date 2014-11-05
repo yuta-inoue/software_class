c34bf6870c93b53a65ca188a7c25f67c089cffc7
yuta-inoue <y.wizowoz@gmail.com>
2014-11-05 18:03:31 +0900

    class4改

--- 004/hw/b.c
+++ 004/hw/b.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
 #include<string.h>
 int counter(char *);
+char trimmed_word[100];
+void trimSpaceEnter(char *);
+
 int main(){
     char word[] = "This is a pen.";
-    int i = 0;
-    i = counter(word);
+    int i = 0,j;
+    char trimmed_word[100]="";
+    for(j=0;j<sizeof(word);j++){
+        char w = word[j];
+        if(!(w ==' '||w=='\n'||w=='\t')){
+            sprintf(trimmed_word,"%s%c",trimmed_word,w);
+        }
+    }
+    i = counter(trimmed_word);
     printf("The number of character is %d\n",i);
     return 0;
 }
