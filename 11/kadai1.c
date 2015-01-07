f503dab0f8d0219214046a1e6b86c59f7c73c57a
井上　裕太 <y.wizowoz@gmail..com>
2015-01-07 15:14:07 +0900

    11 課題完了

--- 11/kadai1.c
+++ 11/kadai1.c
@@ -1,34 +1,35 @@
 #include<stdio.h>
-#include</* TODO 1 */>
+#include<stdlib.h>
 
 void alloc1(int num){
   double *x, s=0.0;
   int i;
   char line[40];
 
-  x = /* TODO 2 */;
-  
+  x = (double*)malloc(sizeof(double)*num);
+
   for(i=0; i<num; i++){
     fgets(line, sizeof(line), stdin);
-    sscanf(line, "%lf", /* TODO 3*/);
+    sscanf(line, "%lf",(x+i) /* TODO 3*/);
   }
-  
+
   for(i=0; i<num; i++){
     s += x[i];
   }
   s /= (double)num;
   printf("%f\n", s);
 
+  free(x);
   /* TODO 4 */;
 }
 
 int main(void){
   int num;
   char line[40];
-  
+
   printf("num of data: ");
   fgets(line, sizeof(line), stdin);
-  sscanf(line, "%d", &num);  
+  sscanf(line, "%d", &num);
 
   alloc1(num);
 
